df0d53a526bfa25003baaa5a5edf487aca818ff8
ArtBern <artem.bernadsky@gmail.com>
2013-02-11 18:58:30 +0400

    ext js form

--- liferay-plugins-sdk-6.1.1/portlets/pervonah-portlet/docroot/WEB-INF/src/org/unido/eetdb/pervonah/MainController.java
+++ liferay-plugins-sdk-6.1.1/portlets/pervonah-portlet/docroot/WEB-INF/src/org/unido/eetdb/pervonah/MainController.java
@@ -1,36 +1,48 @@
 package org.unido.eetdb.pervonah;
 
+import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 @Controller
 @RequestMapping("view")
-public class MainController 
-{
+public class MainController {
 
 	@RenderMapping
-	public String setModelAndView()
-	{
+	public String setModelAndView() {
 		return "page1";
 	}
-	
+
 	@RenderMapping(params = "action=showPage2")
-	public String showPage2() 
-	{
+	public String showPage2() {
 		return "page2";
 	}
-	
-	@ActionMapping(params="action=doAction")
-	public void doAction(ActionResponse response) 
-	{
-		
+
+	@ActionMapping(params = "action=doAction")
+	public void doAction(ActionResponse response) {
+
 		System.out.println("Congratulations, you just did something.");
-		
+
 		response.setRenderParameter("action", "showPage2");
+
+	}
+
+	@ActionMapping(params = "action=doSimpleForm")
+	public void changePreferences(@RequestParam String first,
+			@RequestParam String last,
+			@RequestParam String company,
+			@RequestParam String email,
+			@RequestParam(required = false) String time,
+			ActionRequest actionRequest) 
+	{
+	
+		System.out.println(first);
 		
 	}
+
 }
