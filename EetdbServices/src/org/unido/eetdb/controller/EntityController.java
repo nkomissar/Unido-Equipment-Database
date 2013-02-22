41714c20fbf58da7fd121753e04b65aaeee2e3ea
nkomissar <nikolaykomissarenko@gmail.com>
2013-02-22 16:50:53 +0400

    fixed template creation

--- EetdbServices/src/org/unido/eetdb/controller/EntityController.java
+++ EetdbServices/src/org/unido/eetdb/controller/EntityController.java
@@ -43,11 +43,4 @@ public class EntityController
     {
         return dataAccessService.getRootTopics();
     }
-    
-    @RequestMapping(method = RequestMethod.POST, value = "/topic")
-    public @ResponseBody
-    Topic createTopic(@PathVariable Topic topic)
-    {
-        return null;
-    }
 }
--- EetdbServices/src/org/unido/eetdb/controller/TemplateController.java
+++ EetdbServices/src/org/unido/eetdb/controller/TemplateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,7 +32,7 @@ public class TemplateController
     
     @RequestMapping(method = RequestMethod.POST, value = "/template")
     public @ResponseBody
-    EntityTemplate createTemplate(@PathVariable EntityTemplate template)
+    EntityTemplate createTemplate(@RequestBody EntityTemplate template)
     {
         return dataAccessService.createEntityTemplate(template);
     }
