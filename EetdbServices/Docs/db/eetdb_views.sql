88198dd0d7a8f4aca025a637de0f9ef284333d80
eeremina <eeremina@MSK-1118.sts.sitronics.com>
2013-03-07 18:27:53 +0400

    docs

---
+++ EetdbServices/Docs/db/eetdb_views.sql
@@ -0,0 +1,10 @@
+-- -----------------------------------------------------
+-- View `eetdb`.`V_ROOT_TOPIC`
+-- -----------------------------------------------------
+
+CREATE OR REPLACE VIEW V_ROOT_TOPIC AS
+  SELECT *
+    FROM UNIDO_TOPIC ut
+   WHERE not exists (SELECT 1 
+                       FROM UNIDO_TOPIC_LINK utl
+                      WHERE utl.LINKED_TOPIC_ID = ut.TOPIC_ID);
