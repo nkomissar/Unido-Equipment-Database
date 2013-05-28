119367c1af7fc8cc25e3ec2cf032e21b8975042c
Nik <Nik@192.168.1.33>
2013-05-28 09:37:38 +0400

    clean up

--- EetdbServices/src/org/unido/eetdb/controller/SearchController.java
+++ EetdbServices/src/org/unido/eetdb/controller/SearchController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.jasper.util.Enumerator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
