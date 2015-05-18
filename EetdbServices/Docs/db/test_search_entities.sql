b369c5c642fb35b45ff37a8231d757f9f2b5513d
Ekaterina <ekaterina.eremina@gmail.com>
2015-05-18 19:25:03 +0300

    more pictures
    social icons

---
+++ EetdbServices/Docs/db/test_search_entities.sql
@@ -0,0 +1,12 @@
+call SEARCH_ENTITIES(
+	'ENGINE', 
+	'<properties>
+		<property>
+			<code>PI</code>
+			<value/>
+			<min>30</min>
+			<max>40</max>
+		</property>
+	</propeUNIDO_ENTITYrties>');
+
+call SEARCH ('дрыг');
